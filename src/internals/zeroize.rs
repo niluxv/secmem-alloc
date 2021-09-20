65833408e2c5ce6d472703e9badd118d17b6fec3
niluxv <niluxv.opensource.C-h2ty6xl@yandex.com>
2021-09-20 20:59:47 +0200

    Fix `libc::explicit_bzero` not being available on windows targets with gnu libc

--- src/internals/zeroize.rs
+++ src/internals/zeroize.rs
@@ -39,7 +39,7 @@ pub unsafe fn volatile_memset(ptr: *mut u8, val: u8, len: usize) {
     target_os = "freebsd",
     target_os = "dragonfly",
     target_os = "openbsd",
-    target_env = "gnu",
+    all(target_env = "gnu", unix),
     target_env = "musl"
 ))]
 pub unsafe fn libc_explicit_bzero(ptr: *mut u8, len: usize) {
@@ -47,6 +47,30 @@ pub unsafe fn libc_explicit_bzero(ptr: *mut u8, len: usize) {
     // SAFETY: the caller must uphold the safety contract
     unsafe {
         libc::explicit_bzero(ptr as *mut libc::c_void, len as libc::size_t);
+    }
+}
+
+/// Overwrite memory with zeros. This operation will not be elided by the
+/// compiler.
+///
+/// This uses the `explicit_bzero` function present in many recent libcs.
+///
+/// # Safety
+/// It's C. But the safety requirement is quite obvious: The caller *must*
+/// ensure that `ptr` is valid for writes of `len` bytes, see the [`std::ptr`]
+/// documentation. In particular this function is not atomic.
+// In addition `ptr` needs to be properly aligned, but because we are talking
+// about bytes (therefore byte alignment), it *always* is.
+#[cfg(all(target_env = "gnu", windows))]
+pub unsafe fn libc_explicit_bzero(ptr: *mut u8, len: usize) {
+    precondition_memory_range!(ptr, len);
+    extern "C" {
+        fn explicit_bzero(ptr: *mut libc::c_void, len: libc::size_t);
+    }
+
+    // SAFETY: the caller must uphold the safety contract
+    unsafe {
+        explicit_bzero(ptr as *mut libc::c_void, len as libc::size_t);
     }
 }
 
